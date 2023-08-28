18f406c37b721c10924b3b9e476f1f304c3e33ac
RaulSMuniz <raulanthonymuniz@gmail.com>
2023-08-28 16:25:10 -0300

    Git

---
+++ src/main/java/com/mycompany/maiornumero/Maiornumero.java
@@ -0,0 +1,12 @@
+package com.mycompany.maiornumero;
+
+public class Maiornumero {
+
+    public static void main(String[] args) {
+        Maiornumero2 num = new Maiornumero2();
+        num.num1 = 13;
+        num.num2 = 12;
+        num.num3 = 11;
+        num.print();
+    }
+}
---
+++ src/main/java/com/mycompany/maiornumero/Maiornumero2.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.maiornumero;
+
+public class Maiornumero2 {
+    public double num1, num2, num3, maior;
+    public double numeros(){
+        // verifica se num1 é maior que num2
+        if (num1 > num2){
+            if (num1 > num3){ // verifica se num1 é maior que num2
+                maior = num1;
+            } else{ // se num1 é menor que num3, num3 é o maior
+                maior = num3;
+            }
+        } else if (num2 > num3){ // se num1 for menor que num2, verifica se num2 é maior que num3
+            maior = num2;
+        } else { // se num2 for menor que num3, num3 é maior
+            maior = num3;
+        }
+    return maior;
+    }
+    public void print(){
+        numeros();
+        System.out.println("O maior número é: "+maior);
+    }
+}
+
