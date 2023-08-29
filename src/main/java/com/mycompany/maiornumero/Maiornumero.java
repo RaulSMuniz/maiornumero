785d1830c7dbdf35def63493c917a070227ac391
Raul Muniz <136410938+RaulSMuniz@users.noreply.github.com>
2023-08-28 22:22:33 -0300

    Maiornumero.java
    adicionados scanners

--- src/main/java/com/mycompany/maiornumero/Maiornumero.java
+++ src/main/java/com/mycompany/maiornumero/Maiornumero.java
@@ -1,12 +1,23 @@
 package com.mycompany.maiornumero;
-
+import java.util.Scanner;
 public class Maiornumero {
 
     public static void main(String[] args) {
         Maiornumero2 num = new Maiornumero2();
-        num.num1 = 13;
-        num.num2 = 12;
-        num.num3 = 11;
+        Scanner n1 = new Scanner(System.in);
+        System.out.println("Digite o primeiro número:");
+        num.num1 = (n1.nextInt());
+
+        Scanner n2 = new Scanner(System.in);
+        System.out.println("Digite o segundo número:");
+        num.num2 = (n2.nextInt());
+
+        Scanner n3 = new Scanner(System.in);
+        System.out.println("Digite o terceiro número:");
+        num.num3 = (n3.nextInt());
         num.print();
+        n1.close();
+        n2.close();
+        n3.close();
     }
 }
